8407334197e41476113a8dd0cd650ed6f510f310
metaleap <metaleap@users.noreply.github.com>
2018-06-30 11:24:30 +0200

    [autopush] «There was a phone call for you.»

--- placeholders.go
+++ placeholders.go
@@ -8,8 +8,8 @@ import (
 // `strings.Replace` / `Replacer` for some (not all) "micro-templating" use-cases.
 func NamedPlaceholders(begin byte, end byte) func(string, ...string) string {
 	return func(s string, namesAndValues ...string) string {
-		if len(s) >= 3 && len(namesAndValues) > 0 {
-			var sb strings.Builder
+		if len(s) >= 3 && len(namesAndValues) > 1 {
+			var buf strings.Builder
 			start, pos := 0, -1
 			for i := 0; i < len(s); i++ {
 				if s[i] == begin {
@@ -23,8 +23,8 @@ func NamedPlaceholders(begin byte, end byte) func(string, ...string) string {
 						}
 					}
 					if nidx >= 0 && nidx < len(namesAndValues)-1 {
-						sb.WriteString(s[start:pos])
-						sb.WriteString(namesAndValues[nidx+1])
+						buf.WriteString(s[start:pos])
+						buf.WriteString(namesAndValues[nidx+1])
 						start = i + 1
 					} else {
 						pos = -1
@@ -32,10 +32,10 @@ func NamedPlaceholders(begin byte, end byte) func(string, ...string) string {
 				}
 			}
 			if start > 0 {
-				sb.WriteString(s[start:])
+				buf.WriteString(s[start:])
 			}
-			if sb.Len() > 0 {
-				return sb.String()
+			if buf.Len() > 0 {
+				return buf.String()
 			}
 		}
 		return s
